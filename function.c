7a4fc39b8868391257c739801e4354ad76f7b03c
rlecomte19 <romanlecomte@gmail.com>
2021-10-02 11:42:00 +0200

    bintodec

---
+++ function.c
@@ -0,0 +1,23 @@
+int bintodec(char *val){
+    int index = sizeof(val) - 1;
+    int dec = val[index];
+
+    for(int i=index-1;i>=0;i--){
+        int base = 1;
+        int j=index;
+
+        do{
+            base *= 2;
+            j--;
+        }while(j>i);
+       
+        dec += val[i] * base;
+        int test = val[i] * base;
+        printf("\nBase a ce moment : %d",base);
+        printf("\nCalcul a ce moment : %d",test);
+        printf("\nValeur a ce moment : %d ", val[i]);
+        printf("\nDecimal a ce moment : %d", dec);
+        printf("\n##########\n");
+    }
+    return dec;
+}
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "fonctions.c"
+
+#pragma region
+int bintodec(char *val);
+int hexatodec(char *val);
+char *dectohexa(unsigned int val);
+char *dectobin_v1(unsigned int val);
+char *dectobin_v2(unsigned int val);
+char *dectobin_v3(unsigned int val);
+char *dectohexa_v2(unsigned int val);
+#pragma endregion
+
+int main(int argc, char *argv[]){
+    char val[8] =  { 1,0,1,0,1,1,0,1 };
+    int valsize = sizeof(val);
+
+    int decimal = bintodec(val);
+
+    printf("Décimal converti : %d", decimal);
+}
+
+
+
