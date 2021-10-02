5fad563569fa4c3df684160f5edd8296e535423e
rlecomte19 <romanlecomte@gmail.com>
2021-10-02 13:24:15 +0200

    dectobin2 correct

--- main.c
+++ main.c
@@ -21,7 +21,11 @@ int main(int argc, char *argv[]){
     char *tobin = dectobin_v2(205);
 
     // printf("Binaire converti 'dectobin' : %d", decimal);
-    printf("Decimal converti 'bintodecv2 : %s\n", tobin);
+    printf("Decimal converti 'bintodecv2 : ");
+    for(int i=(sizeof(tobin)-1); i>=0;i--){
+        printf("%d", tobin[i]);
+    }
+    printf("\n");
 }
 
  
